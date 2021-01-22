9614f4aaa682b42d4dc42ee79bc4756bb926a084
nakagawa <237s10mimi@gmail.com>
2021-01-22 16:54:10 +0900

    feat: add Application class

---
+++ original-framework/application/core/Application.php
@@ -0,0 +1,78 @@
+<?php
+  abstract class Application
+  {
+    protected $debug = false;
+    protected $request;
+    protected $renponse;
+    protected $session;
+    protected $db_manager;
+
+    public function __construct($debug = false) {
+      $this->setDebugMode($debug);
+      $this->initialize();
+      $this->configure();
+    }
+
+    public function isDebugMode() {
+      return $this->debug;
+    }
+
+    public function getRequest() {
+      return $this->request;
+    }
+
+    public function getResponse() {
+      return $this->response;
+    }
+
+    public function getsession() {
+      return $this->session;
+    }
+
+    public function getDbManager() {
+      return $this->db_manager;
+    }
+
+    public function getControllerDir() {
+      return $this->getRootDir() . '/controllers';
+    }
+
+    public function getViewDir() {
+      return $this->getRootDir() . '/views';
+    }
+
+    public function getModelDir() {
+      return $this->getRootDir() . '/models';
+    }
+
+    public function getWebDir() {
+      return $this->getRootDir() . '/web';
+    }
+
+    protected function setDebugMode($debug) {
+      if($debug) {
+        $this->debug = true;
+        ini_set('display_errors', 1);
+        error_reporting(-1);
+      } else {
+        $this->debug = false;
+        ini_set('display_errors', 0);
+      }
+    }
+
+    protected function initialize() {
+      $this->request = new Request();
+      $this->response = new Response();
+      $this->session = new Session();
+      $this->db_manager = new DbManager();
+      $this->router = new Router($this->registerRoutes());
+    }
+
+    protected function configure() {
+
+    }
+
+    abstract public function getRootDir();
+
+    abstract public function registerRoutes();
+  }
