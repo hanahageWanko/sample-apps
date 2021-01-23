998c9a399fb0e0ef7ef7f4ae94803b6f3244b8d5
nakagawa <237s10mimi@gmail.com>
2021-01-23 23:16:25 +0900

    feat: layout.php

---
+++ miniblog/application/views/layout.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title><?php if(isset($title)){ echo $this->escape($title) . ' - '; } ?>Mini Blog</title>
+</head>
+<body>
+  <div id="header">
+    <h1>
+    <a href="<?php echo $base_url; ?>">Mini Blog</a></h1>
+  </div>
+
+  <div id="main">
+    <?php echo $_content; ?>
+  </div>
+</body>
+</html>
